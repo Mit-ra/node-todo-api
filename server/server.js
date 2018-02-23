e51b0b859a1686382edf0402e4feef43083ed3da
krina <kinare92@gmail.com>
2018-02-23 07:13:39 +0530

    Add POST/user route and token method

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+var env = process.env.NODE_ENV;
+
 const _ = require('lodash');
 
 const express = require('express');
@@ -106,6 +108,20 @@ app.patch('/todos/:id', (req, res) => {
 
     }).catch((e) => {
         res.status(400).send();
+    })
+});
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user =  new User(body);
+
+    user.save().then(() => {
+       return user.generateAuthToken();
+        // res.send(user);
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
     })
 });
 
